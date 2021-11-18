21c81287b7ea924fedede6bb11ab00bfb93266d6
Ananya <avagadi15@gmail.com>
2021-11-18 20:06:01 +0530

    edit/show/del page fix for heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   def index
     @users = User.all
   end
@@ -15,8 +15,28 @@ class UsersController < ApplicationController
   def show
   end
 
-  private
+  # def edit
+  #   @user = User.find(params[:id])
+  # end
+  
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: "User was successfully updated." }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
+  private
+  
+  def set_user
+    @user = User.find(params[:id])    
+  end
+  
   def user_params
     params.require(:user).permit(:name, :phone, :email)
   end
