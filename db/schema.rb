a57e36538d0569a4dcac22e2fe0e1eb377452813
Ananya <avagadi15@gmail.com>
2021-11-19 22:32:02 +0530

    Orders model & controllers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_18_064618) do
+ActiveRecord::Schema.define(version: 2021_11_19_133619) do
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "product_id"
+    t.decimal "total"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
